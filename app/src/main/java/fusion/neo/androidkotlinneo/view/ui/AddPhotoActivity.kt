fea60d0930d0eca4718b876dc2b34a5b86c54543
farhan <pahlevi.kun@gmail.com>
2018-01-19 22:48:49 +0700

    Add AddPhotoActivity & MainActivityLayout

---
+++ app/src/main/java/fusion/neo/androidkotlinneo/view/ui/AddPhotoActivity.kt
@@ -0,0 +1,13 @@
+package fusion.neo.androidkotlinneo.view.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import fusion.neo.androidkotlinneo.R
+
+class AddPhotoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_photo)
+    }
+}
--- app/src/main/java/fusion/neo/androidkotlinneo/view/ui/MainActivity.kt
+++ app/src/main/java/fusion/neo/androidkotlinneo/view/ui/MainActivity.kt
@@ -1,8 +1,11 @@
 package fusion.neo.androidkotlinneo.view.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import fusion.neo.androidkotlinneo.R
 
@@ -23,5 +26,25 @@ class MainActivity : AppCompatActivity() {
         this.doubleBackToExitPressedOnce = true
         Toast.makeText(this, getString(R.string.app_login_double_back), Toast.LENGTH_SHORT).show()
         Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.add, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+
+        if (id == android.R.id.home) {
+            finish()
+            return true
+        } else if (id == R.id.action_add) {
+            startActivity(Intent(this, AddPhotoActivity::class.java))
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
